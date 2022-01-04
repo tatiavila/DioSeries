9b793d660a412d7303348093ffda832648b01c41
tatiavila <tatiplacebo@gmail.com>
2022-01-04 19:45:44 -0400

    Cadastro de Series

---
+++ DioSeries/Classes/Serie.cs
@@ -0,0 +1,58 @@
+﻿using DioSeries.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DioSeries
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno +="Genero: " + Genero + Environment.NewLine;
+            retorno +="Titulo: " + Titulo + Environment.NewLine;
+            retorno +="Descricao: " + Descricao + Environment.NewLine;
+            retorno +="Ano de inicio: " + Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int retornaId()
+        {
+            return Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return Excluido;
+        }
+
+        public void Excluir()
+        {
+            Excluido = true;
+        }
+    }
+}
